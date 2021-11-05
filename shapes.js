b4b9eafe2917d9f3754f25182b1d88ba147a898e
pickepan <55358214+pickepan@users.noreply.github.com>
2021-11-05 15:53:03 +0100

    Added a chart that parses data from a csv

---
+++ shapes.js
@@ -0,0 +1,31 @@
+d3.select(".myclass").style("color", "red"); //colorize text in red
+d3.selectAll(".myclass").transition().style("color","green").duration(5000); //change it to green
+		 
+var width = 300;
+var height = 300;
+ //Create SVG element
+var rectangle = d3.select("#svgcontainer")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
+			
+var circle = d3.select("#svgcontainer")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
+			
+			
+         //append rectangle element
+rectangle.append("rect")
+            .attr("x", 20)
+            .attr("y", 20)
+            .attr("width", 200)
+            .attr("height", 100)
+            .attr("fill", "green");
+		 
+         //append circle 
+circle.append("circle")
+            .attr("cx", 200)
+            .attr("cy", 50)
+            .attr("r", 20)
+            .attr("fill", "blue");
